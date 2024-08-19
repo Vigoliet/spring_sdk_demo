62786ef2cf451da9f520fc5733ddfc528da7231b
Vigor Lundkvist <vigor.lundkvist@live.se>
2024-08-19 15:56:59 +0200

    POC of creating bucket with endpoint

--- src/main/java/com/example/spring_sdk_demo/controllers/BucketController.java
+++ src/main/java/com/example/spring_sdk_demo/controllers/BucketController.java
@@ -4,6 +4,8 @@ import com.example.spring_sdk_demo.models.Bucket;
 import com.example.spring_sdk_demo.repositories.BucketRepository;
 import com.example.spring_sdk_demo.s3bucket.S3Example;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +21,21 @@ public class BucketController {
     @Autowired
     private BucketRepository bucketRepository;
 
-    @RequestMapping("/buckets")
+    @GetMapping("/buckets")
     public List<Bucket> getBuckets() {
         S3Example s3Example = new S3Example();
         var buckets = s3Example.listBuckets();
         System.out.println(buckets);
         return buckets;
     }
+
+    // Right now i create the bucket in the request, however this will be a field in the front end
+    @GetMapping("/createBucket{bucketName}")
+    public void createBucket(@PathVariable String bucketName) {
+        S3Example s3Example = new S3Example();
+        s3Example.createBucket(bucketName);
+    }
+
 
 
 
